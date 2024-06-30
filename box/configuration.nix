a9a7f2f47d4baba5f8ba4b5364430d2e1d4eea8b
Samuel Mathews <mathews.sam10@gmail.com>
2024-06-30 16:27:42 -0400

    Making modifications

--- box/configuration.nix
+++ box/configuration.nix
@@ -66,8 +66,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-  
-  nix.nixPath = [ "/home/samuel/nixos-config/box-configuration.nix" ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.samuel = {
