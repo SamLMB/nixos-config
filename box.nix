0b8b84c1099928562ad3eed957636b4efa3edcf0
Samuel Mathews <mathews.sam10@gmail.com>
2024-06-30 13:11:55 -0400

    Updating some configs

--- box.nix
+++ box.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./box-hardware-configuration.nix
     ];
 
   # Bootloader.
--- lenovo.nix
+++ lenovo.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./lenovo-hardware-configuration.nix
     ];
 
   # Bootloader.
